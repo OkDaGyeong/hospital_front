3ee49582a4a3612c1fc0d9ef42dcd39025aaa9d9
OkDaGyeong <81674153+OkDaGyeong@users.noreply.github.com>
2023-07-05 23:21:18 +0900

    nav

--- frontend/src/containers/common/Header.jsx
+++ frontend/src/containers/common/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logoImg from "../../static/images/logoIcon.png";
+import logoImg from "../../static/images/logoWhite.png";
 
 import "../../styles/common/Header.scss";
 import Container from "react-bootstrap/Container";
@@ -11,7 +11,7 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 function Header() {
   return (
     <>
-      <Navbar expand={false} className="mb-3 bg-navy">
+      <Navbar expand={false} className="mb-3 navStyle">
         <Container fluid>
           <Navbar.Brand href="/patient-list">
             <img src={logoImg} alt="아이콘" style={{ height: "30px" }} />
@@ -33,13 +33,24 @@ function Header() {
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="/patient-list">환자 목록</Nav.Link>
-                <NavDropdown
-                  title="31병동"
-                  id={`offcanvasNavbarDropdown-expand-${31}`}
-                >
-                  <NavDropdown.Item href="#">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#">Another action</NavDropdown.Item>
-                </NavDropdown>
+
+                {[
+                  "31병동",
+                  "51병동",
+                  "61병동",
+                  "62병동",
+                  "71병동",
+                  "72병동",
+                ].map((expand) => (
+                  <NavDropdown
+                    key={expand}
+                    title={expand}
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#">환자1</NavDropdown.Item>
+                    <NavDropdown.Item href="#">환자2</NavDropdown.Item>
+                  </NavDropdown>
+                ))}
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
